6618aacb7b3d1b8188aaef0939a22d131d7d9b48
bshelor <bryson_shelor@taylor.edu>
2019-04-23 20:42:47 -0400

    search, subtree, sumtree

--- A04/tree.pl
+++ A04/tree.pl
@@ -88,6 +88,8 @@ tree4(
         )
 ).
 
+% tree(4,tree(5,void,void),tree(9,void,void))
+
 tree5(
         tree(   3,
                 tree(   5,
@@ -99,27 +101,43 @@ tree5(
                                 tree(5,void,void),
                                 tree(9,void,void)
                         )
+                ),
+                tree(   4,
+                        tree(   8,
+                                tree(6,void,void),
+                                tree(4,void,void)
+                        ),
+                        tree(   7,
+                                tree(8,void,void),
+                                tree(3,void,void)
+                        )
                 )
         )
 ).
+
+% tree6(tree(3,tree(5,tree(   9,tree(7,void,void),tree(9,void,void)),tree(   4,tree(5,void,void),tree(9,void,void))),tree(   4,tree(   8,tree(6,void,void),tree(4,void,void)),tree(   7,tree(8,void,void),tree(3,void,void))))).
 
 % preorder(tree(X,L,R),Xs) :- preorder(L,Ls), preorder(R,Rs),
 %                             append([X|Ls],Rs,Xs).
 % preorder(void,[]).
 
 % inorder
+inorder(tree(X,L,R),Xs) :- inorder(L,Ls), inorder(R,Rs), append(Ls,[X|Rs],Xs).
 inorder(void, []).
-inorder(tree(X,L,R),Xs) :- inorder(L,Ls), inorder(R,Rs),
-                        append()
 
 % search
-
+search(tree(Key,_,_), Key).
+search(tree(_,Left,_), Key) :- search(Left, Key).
+search(tree(_,_,Right), Key) :- search(Right, Key).
 
 % subtree
-
+subtree(T,T).
+subtree(T, tree(_,L,_)) :- subtree(T,L).
+subtree(T, tree(_,_,R)) :- subtree(T,R).
 
 % sumtree
-
+sumtree(void, 0).
+sumtree(tree(X,L,R), S) :- sumtree(L, SumLeft), sumtree(R, SumRight), S is X+SumLeft+SumRight.
 
 % ordered
 
