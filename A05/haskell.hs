18d834fe086510128a1335d21a562dae4ef95162
bshelor <bryson_shelor@taylor.edu>
2019-04-29 09:49:46 -0400

    Starting Haskell assignment

---
+++ A05/haskell.hs
@@ -0,0 +1,13 @@
+-- Possible prods implementation
+-- prods :: Int -> [Int]
+-- prods n = [n*n, (n*n)+n ..]
+
+-- Possible firstn implementation
+-- firstn :: Int -> [Int]
+-- firstn n = sieve [2..primesto n][]
+
+-- Possible primesto implementation
+-- primesto :: Int -> [Int]
+-- primesto n | n < 2 = []
+--             | sieve [2..n][]
+
